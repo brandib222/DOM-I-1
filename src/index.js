3712311d97d6a537318ddddb15824a2d9ff4e50c
Greg Ball <gregball@Gregs-MBP.attlocal.net>
2021-11-30 10:51:17 -0500

    images and footer link complete

--- src/index.js
+++ src/index.js
@@ -38,72 +38,31 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "accent-img": "mocks/img/accent.png",
   },
 };
-// select all of the navs
-const siteNav1 = document.querySelector('nav');
-const links = document.querySelectorAll('nav a');
 
-// select elements of the top
-const siteTitle = document.querySelector('.cta h1');
-const siteButton = document.querySelector('.cta button');
+// IMAGES
 
-// select elements of the feature section
-const featuresHeader = document.querySelector('h4:nth-of-type(1)');
-const featuresContent = document.querySelector('p:nth-of-type(1)');
+// select first image
+const logoImg = document.querySelector('#logo-img');
+// update first image
+logoImg.src = siteContent.images['logo-img'];
 
-// select elements of the about section
-const aboutHeader = document.querySelector('h4:nth-of-type(2)');
-const aboutContent = document.querySelector('p:nth-of-type(2)');
+// select second image
+const ctaImg = document.querySelector('#cta-img');
+// update second image 
+ctaImg.src = siteContent.images['cta-img'];
 
-// select elements of services section
-const servicesHeader = document.querySelector('h4:nth-of-type(3)');
-const servicesContent = document.querySelector('p:nth-of-type(3)');
+// select third image
+const accentImg = document.querySelector('#middle-img');
+accentImg.src = siteContent.images['accent-img'];
+console.log(accentImg);
 
-// select elements of the product section
-const productHeader = document.querySelector('h4:nth-of-type(4)');
-const productContent = document.querySelector('p:nth-of-type(4)');
+// FOOTER LINK
+const footerLink = document.querySelector('footer a');
+footerLink.textContent = siteContent.footer.copyright;
+footerLink.classList.add('bold');
 
-// select elements of vision section
-const visionHeader = document.querySelector('h4:nth-of-type(5)');
-const visionContent = document.querySelector('p:nth-of-type(5)');
-
-// select elements of contact section
-const contactHeader = document.querySelector('h4:nth-of-type(6)');
-const contactAddress = document.querySelector('p:nth-of-type(7)');
-const contactPhone = document.querySelector('p:nth-of-type(8)');
-const contactEmail = document.querySelector('p:nth-of-type(9)')
-
-// select all elements of footer section
-const siteFooter = document.querySelector('footer');
-
-// select all elements of image section
-const logoImg = document.querySelector('.logo-img');
-const ctaImg = document.querySelector('.cta-img');
-const accentImg = document.querySelector('.accent-img');
-
-// append some stuff ???
-//document.querySelector('h4').appendChild(featuresHeader);
-
-// update the text content for top part
-siteTitle.textContent = 'DOM Is Awesome';
-siteButton.textContent = 'Get Started';
-
-// update text content for features
-featuresHeader.textContent = 'Features';
-featuresContent.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-
-//update content for about
-aboutHeader.textContent = 'About';
-aboutContent.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
-
-//update content for services
-
-//update content for product
-
-//update content for vision
-
-//update content for contact
-
-siteFooter.textContent = 'Copyright Great Idea! 2021';
-
+// CONTACT
+const contact = document.querySelector('section.contact');
+console.log(contact);
 
 console.log('project wired!')
